b3c53e59bb89bfc990a8b935cbd7cefb1f3e9535
Ricardo Tavares <rnunot@gmail.com>
2019-11-26 15:00:08 +0000

    Update schema

--- src/db/dml/init.sql
+++ src/db/dml/init.sql
@@ -1,41 +1,41 @@
 create database address_book_api;
 
-create table address_book
+create table if not exists address_book
 (
-    id       serial       not null
-        constraint address_book_pk
-            primary key,
-    username varchar(50)  not null,
-    password varchar(100) not null
+	id serial not null
+		constraint address_book_pk
+			primary key,
+	username varchar(50) not null,
+	password varchar(500) not null
 );
 
-create unique index address_book_username_uindex
-    on address_book (username);
+create unique index if not exists address_book_username_uindex
+	on address_book (username);
 
-create table contact_group
+create table if not exists contact_group
 (
-    id              serial  not null
-        constraint contact_group_pk
-            primary key,
-    name            varchar(100),
-    description     varchar(500),
-    picture_url     varchar(500),
-    address_book_id integer not null
-        constraint contact_group_address_book_id_fk
-            references address_book
+	id varchar(1000) not null
+		constraint contact_group_pk
+			primary key,
+	name varchar(100),
+	description varchar(500),
+	picture_url varchar(500),
+	address_book_id integer not null
+		constraint contact_group_address_book_id_fk
+			references address_book
 );
 
-create table contact
+create table if not exists contact
 (
-    name            varchar(100) not null
-        constraint contact_pk
-            primary key,
-    phone           varchar(200),
-    picture_url     varchar(500),
-    group_id        integer
-        constraint contact_contact_group_id_fk
-            references contact_group,
-    address_book_id integer      not null
-        constraint contact_address_book_id_fk
-            references address_book
+	name varchar(100) not null
+		constraint contact_pk
+			primary key,
+	phone varchar(200),
+	picture_url varchar(500),
+	group_id varchar(1000)
+		constraint contact_contact_group_id_fk
+			references contact_group,
+	address_book_id integer not null
+		constraint contact_address_book_id_fk
+			references address_book
 );

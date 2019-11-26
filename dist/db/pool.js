e1d7589e4e20a3f04d0b2cddce399d705317cd39
Ricardo Tavares <rnunot@gmail.com>
2019-11-26 11:31:04 +0000

    Remove dist, remove postinstall sript

--- dist/db/pool.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const pg_1 = require("pg");
-const connectionString = process.env.DATABASE_URL ||
-    'postgresql://postgres:root@localhost:5432/address_book_api';
-exports.default = new pg_1.Pool({
-    connectionString,
-});
-//# sourceMappingURL=pool.js.map

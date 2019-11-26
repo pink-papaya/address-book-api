950c20223035483bed5df08255d50e97e0d255d4
Ricardo Tavares <rnunot@gmail.com>
2019-11-26 14:16:40 +0000

    Add cors middleware

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import pino from 'express-pino-logger';
+import cors from 'cors';
 import addressBookRouter from '@/routes/addressBook';
 import contactRouter from '@/routes/contact';
 import contactGroupRouter from '@/routes/contactGroup';
@@ -9,8 +10,9 @@ const PORT = process.env.PORT || 5000;
 
 express()
   .use(express.json())
-  .use(pino())
   .use(bodyParser.urlencoded({ extended: true }))
+  .use(cors())
+  .use(pino())
   .use('/', addressBookRouter)
   .use('/', contactRouter)
   .use('/', contactGroupRouter)
